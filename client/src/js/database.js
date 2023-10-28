816ef1d7082ed4a82015ccd8670519a047984601
desktopbuddy <hanchondo21@gmail.com>
2023-10-27 23:22:07 -0700

    post and get functions for db

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,39 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// POST to the database
+export const putDb = async (content) => {
+  console.log('Post to the database');
+  // Database and version
+  const jateDb = await openDB('jate', 1);
+  // New transaction specifying database and data priveleges
+  const tx = jateDb.transaction('todos', 'readwrite');
+  // Open desired object store
+  const store = tx.objectStore('todos');
+  // .add() passes in content
+  const request = store.put({ id: 1, value: content });
+  
+  const result = await request;
+  console.log('result.value ', result);
+  return result;
+};
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+// Get all the content from the database
+export const getDb = async () => {
+  console.log('Get all from the database');
+  // Database and version
+  const jateDB = await openDB("jate", 1);
+  // New transaction specifying db and privileges
+  const tx = jateDB.transaction("jate", "readonly");
+  // Open desired object store
+  const store = tx.objectStore("jate");
+  // Get all request
+  const request = store.getAll();
+  
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+};
+
 
 initdb();
