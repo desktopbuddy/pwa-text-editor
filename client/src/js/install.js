9e4b8e3ec1d86c486c6352d455f12ffab7a4812b
desktopbuddy <hanchondo21@gmail.com>
2023-10-27 23:33:14 -0700

    bugfix install.js

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -10,7 +10,6 @@ window.addEventListener('beforeinstallprompt', (event) => {
 
 // click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
-  event.prompt();
   butInstall.setAttribute('disabled', true);
   butInstall.textContent = 'Installing'
 });
